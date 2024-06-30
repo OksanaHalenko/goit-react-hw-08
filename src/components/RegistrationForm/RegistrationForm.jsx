f8efd33420dc9979e8ca01969dcddab07f09c2b0
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-06-30 10:12:55 +0200

    create registration page

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,74 @@
+import css from "./RegistrationForm.module.css";
+
+import { useId } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+
+function RegistrationForm() {
+  const nameId = useId();
+  const emailId = useId();
+  const passwordId = useId();
+  const registerSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("must be a valid email").required("Required"),
+    password: Yup.string()
+      .min(6, "must be at least 6 characters long")
+      .required("Required"),
+  });
+  const dispatch = useDispatch();
+
+  const handleSubmit = ({ name, email, password }, actions) => {
+    dispatch(register({ name, email, password }));
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{ name: "", email: "", password: "" }}
+      onSubmit={handleSubmit}
+      validationSchema={registerSchema}
+    >
+      <Form className={css.form}>
+        <div className={css.wrapper}>
+          <label className={css.label} htmlFor={nameId}>
+            Name
+          </label>
+          <Field className={css.input} type="text" name="name" id={nameId} />
+          <ErrorMessage className={css.error} name="name" component="span" />
+        </div>
+        <div className={css.wrapper}>
+          <label className={css.label} htmlFor={emailId}>
+            E-mail
+          </label>
+          <Field className={css.input} type="email" name="email" id={emailId} />
+          <ErrorMessage className={css.error} name="email" component="span" />
+        </div>
+        <div className={css.wrapper}>
+          <label className={css.label} htmlFor={emailId}>
+            Password
+          </label>
+          <Field
+            className={css.input}
+            type="password"
+            name="password"
+            id={passwordId}
+          />
+          <ErrorMessage
+            className={css.error}
+            name="password"
+            component="span"
+          />
+        </div>
+        <button className={css.btn} type="submit">
+          Registration
+        </button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default RegistrationForm;
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,5 +1,17 @@
+import css from "./RegistrationPage.module.css";
+
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+
 function RegistrationPage() {
-  return <div>Registration page</div>;
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.title}>Registration</h1>
+      <p className={css.text}>
+        Please register to be able to create your contacts book
+      </p>
+      <RegistrationForm />
+    </div>
+  );
 }
 
 export default RegistrationPage;
